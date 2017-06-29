a99ad4032ce1abb82f9b3c59a9369ebe5132da9b
Christopher Ferris <chrisfer@us.ibm.com>
2017-06-28 21:31:30 -0400

    this time

--- Formula/fabric-tools.rb
+++ Formula/fabric-tools.rb
@@ -5,7 +5,6 @@
 class FabricTools < Formula
   desc "Hyperledger Fabric native binaries installer"
   homepage "https://hyperledger.org/projects/fabric"
-  # TODO pick URL where the MacOSX binaries tarball will be hosted
   url "https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric/darwin-amd64-1.0.0-rc1/hyperledger-fabric-darwin-amd64-1.0.0-rc1.tar.gz"
   sha256 "03f4e9a9ce42c1165376fd0077fe56f76f164f8fb9a51024da46ffad6c42c3d4"
 
